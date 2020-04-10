8071cc6a968084f632f9269057a0a0b6d3f269e0
Navneet Singh <rathore.singhnav@gmail.com>
2020-04-11 00:34:33 +0530

    Created a Chat room

---
+++ Networking/net.js
@@ -0,0 +1,35 @@
+process.stdout.write('\u001B[2J\u001B[0;0f');
+const server = require('net').createServer();
+
+let counter = 0;
+let sockets = {};
+function timeStamp () {
+    const now = new Date();
+    return `${now.getHours()}:${now.getMinutes()}`;
+}
+
+server.on('connection', socket => {
+    socket.id = counter++;
+    console.log('Client Connected');
+    socket.write('Please Enter the name\n');
+    
+    socket.on('data', data => {
+        if(!sockets[socket.id]){
+        socket.name= data.toString().trim();
+        socket.write(`Welcome ${socket.name}\n`);
+        sockets[socket.id]=socket;
+        return;
+        }
+        Object.entries(sockets).forEach(([key, cs]) => {
+            if(socket.id == key) return;
+            cs.write(`${socket.name} ${timeStamp()} : ${data}`);
+        });
+    });
+
+    socket.on('end', () => {
+        delete sockets[socket.id];
+        console.log('Client Disconnected');
+    })
+});
+
+server.listen(8000, () =>  console.log('Server Bound'));
